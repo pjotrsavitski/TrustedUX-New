80c2ec941bd3ba5ca45e39e295f6aada5b4cd20f
pankajchejara23@gmail.com <pankajchejara23@gmail.com>
2020-10-03 16:49:49 +0530

    whitenoise

--- TrustedUX/settings.py
+++ TrustedUX/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
